a29434572d7cad0f0e19c79dc2cd8cd0a9a76aae
Brian Lynch <bglynch17@gmail.com>
2018-06-13 10:56:30 +0000

    password reset added

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SYSTEM_EMAIL = 'support@ex.com'     
